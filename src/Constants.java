eccacb127899424193e0f0563efe89460f338891
oak-and-lion <oakandlionsociety@gmail.com>
2021-01-31 15:11:46 -0500

    updates

--- src/Constants.java
+++ src/Constants.java
@@ -198,4 +198,5 @@ public class Constants {
     public static final int SECONDARY_BUTTON = 2;
     public static final int EQUALS_VALUE = 0;
     public static final String BLOCKED_IMAGE_FILE = "blocked-image.png";
+    public static final String BLOCKED_VIDEO_FILE = "blocked-video.mp4";
 }
--- src/ModalVideoViewer.java
+++ src/ModalVideoViewer.java
@@ -2,7 +2,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -16,6 +19,9 @@ public class ModalVideoViewer extends BaseViewer implements IModalViewer {
 
     public void show(String file) {
         try (InputStream stream = new FileInputStream(file)) {
+            VBox vbox = new VBox();
+            vbox.setLayoutX(50);
+            vbox.setLayoutY(30);
             String f = new File(file).toURI().toString();
             Media media = new Media(f);
             mediaPlayer = new MediaPlayer(media);
@@ -24,9 +30,16 @@ public class ModalVideoViewer extends BaseViewer implements IModalViewer {
             mediaView.setFitWidth(Constants.IMAGE_LARGE_FIT_WIDTH);
             mediaPlayer.setAutoPlay(true);
             mediaPlayer.setVolume(0.1);
+
+            VBox vbox2 = new VBox();
+            Button button = new Button("Pause");
+            button.setPadding(new Insets(0, 0, 0, 0));
+            vbox2.setLayoutX(410);
+            vbox2.getChildren().add(button);
             //setting group and scene   
             Group root = new Group();  
-            root.getChildren().add(mediaView);  
+            vbox.getChildren().addAll(mediaView, vbox2);
+            root.getChildren().addAll(vbox);
             showStage(root, "Video");
         } catch (Exception e) {
             e.printStackTrace();
