d94a08b931f35841a2b5a881104927c282e119f4
oak-and-lion <oakandlionsociety@gmail.com>
2021-02-09 14:21:43 -0500

    updates

--- src/CommandWorkerGetClone.java
+++ src/CommandWorkerGetClone.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class CommandWorkerGetClone extends CommandWorkerBase implements ICommandWorker {
     private IFactory factory;
+
     public CommandWorkerGetClone(IUtility utility, ISquare square, IDialogController parent, IFactory factory) {
         super(utility, square, parent);
         this.factory = factory;
@@ -15,21 +16,28 @@ public class CommandWorkerGetClone extends CommandWorkerBase implements ICommand
         result.add(r);
         return result;
     }
+
     private BooleanString getClone(String[] args) {
-        IClient client = factory.createClient(Constants.BASE_CLIENT, args[1], Integer.valueOf(args[2]), args[3]);
+        String hostName = args[0];
+        int port = Integer.parseInt(args[1]);
+        String squareId = args[2];
+        String password = args[3];
+        IClient client = factory.createClient(Constants.BASE_CLIENT, hostName, port, squareId);
         String pkey = client.sendMessage(Constants.REQUEST_PUBLIC_KEY_COMMAND, false);
         SquareResponse resp = new SquareResponse(pkey);
         if (resp.getCode().equals(Constants.OK_RESULT)) {
             ISquareKeyPair keyPair = factory.createSquareKeyPair(Constants.UTILITY_SQUARE_KEY_PAIR, utility);
             keyPair.setPublicKeyFromBase64(resp.getMessage());
             String key = utility.generateRandomString(Constants.ENCRYPTION_KEY_LENGTH);
-            String e = utility.encrypt(Constants.CLONE_COMMAND + Constants.COMMAND_DATA_SEPARATOR + args[4], key);
+            String e = utility.encrypt(Constants.CLONE_COMMAND + Constants.COMMAND_DATA_SEPARATOR + password
+                    + Constants.COMMAND_DATA_SEPARATOR + utility.readFile(Constants.IP_FILE)
+                    + Constants.COMMAND_DATA_SEPARATOR + utility.readFile(Constants.PORT_FILE), key);
             String f = keyPair.encryptToBase64(key);
             String result = client.sendMessage(f + Constants.COMMAND_DATA_SEPARATOR + e, true);
             SquareResponse cloneResponse = new SquareResponse(result);
             if (cloneResponse.getCode().equals(Constants.OK_RESULT)) {
-                StringBuilder pwdPadded = new StringBuilder(args[4]);
-                for (int x = args[4].length(); x < Constants.ENCRYPTION_KEY_LENGTH; x++) {
+                StringBuilder pwdPadded = new StringBuilder(password);
+                for (int x = password.length(); x < Constants.ENCRYPTION_KEY_LENGTH; x++) {
                     pwdPadded.append(Constants.UNDERSCORE);
                 }
                 String b64 = utility.decrypt(cloneResponse.getMessage(), pwdPadded.toString());
