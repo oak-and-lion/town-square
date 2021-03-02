e9e008d186ee3c866fa16a0ca1b1d319ea804c91
oak-and-lion <oakandlionsociety@gmail.com>
2021-03-02 10:09:07 -0500

    send file update

--- src/SquareWorkerSendAttachment.java
+++ src/SquareWorkerSendAttachment.java
@@ -5,7 +5,7 @@ public class SquareWorkerSendAttachment extends SquareWorkerBase implements ISqu
     }
     public SquareResponse doWork(ISquare square, String[] args) {
         String[] attachment = args[3].split(Constants.FILE_DATA_SEPARATOR);
-        utility.writeBinaryFile(attachment[1], utility.convertFromBase64(attachment[0]));
+        utility.writeBinaryFile(attachment[0], utility.convertFromBase64(attachment[1]));
         return new SquareResponse(Constants.OK_RESULT, command);
     }
 }
